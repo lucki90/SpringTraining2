45c3e21db659bf41a7c36b8d649c4d9a933dc338
lucki90 <tomlucki@wp.pl>
2019-06-10 18:30:48 +0200

    [springTraining2] - singleton/prototype training

--- src/main/java/SpringDiApplication.java
+++ src/main/java/SpringDiApplication.java
@@ -1,2 +1,20 @@
+import beans.MessagePrinter;
+import beans.MessageProducer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
 public class SpringDiApplication {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(SpringDiApplication.class);
+
+        MessagePrinter bean1 = ctx.getBean(MessagePrinter.class);
+        bean1.printeMessage();
+        MessagePrinter bean2 = ctx.getBean(MessagePrinter.class);
+        bean2.printeMessage();
+
+        ctx.close();
+    }
 }
